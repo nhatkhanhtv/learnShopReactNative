c27b9570246abdc93e843732a0d9c566949d0d3f
why\accklt <nhatkhanhtv@gmail.com>
2019-11-17 23:06:46 +0700

    an header, them button nhung chua hien drawer dc

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,13 +1,36 @@
 import React,{Component} from 'react';
-import {View,Text,Button} from 'react-native';
+import {View,Text,Button,Image,StyleSheet,TouchableOpacity} from 'react-native';
 
-
+import { Icon } from 'react-native-vector-icons/FontAwesome'; // 6.2.2
 import Menu from './Menu';
 import Shop from './Shop/Shop';
 
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  },
+})
+
+
+
+// const LogoTitle extends React.Component {
+//   render() {
+//     return (
+//       <Image
+//         source={require('./spiro.png')}
+//         style={{ width: 30, height: 30 }}
+//       />
+//     );
+//   }
+// }
+
 class Main extends Component {
-
-
+  // static navigationOptions = {
+  //   title:"Home",
+  //   headerMode: 'none',
+  // }
 
   goToMenu(){
     this.props.navigation.openDrawer();
@@ -21,8 +44,6 @@ class Main extends Component {
     const {navigate} = this.props.navigation;
     return (
       <>
-
-
       <Button
         onPress={() => {this.goToMenu()}}
         title="Go to Menu"
